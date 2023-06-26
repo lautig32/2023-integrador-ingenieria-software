06684fc6beeb97ccdb3228ec72cd993d52d2db3b
lautaro-g <lautigomez32@gmail.com>
2023-06-26 10:55:55 -0300

    Nombre de usuario o contraseña incorrectos.

--- apps/football_system/views.py
+++ apps/football_system/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def home(request):
     
     return render(request, 'home.html')
 
-def login(request):
-    
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home') 
+        else:
+            message = 'Nombre de usuario o contraseña incorrectos.'
+
+            context = {
+                'message': message
+            }
+
+            return render(request, 'login.html', context)
+
     return render(request, 'login.html')
 
 def signup(request):
@@ -13,8 +30,13 @@ def signup(request):
     return render(request, 'signup.html')
 
 def profile(request):
-    
-    return render(request, 'profile.html')
+    user = request.user
+
+    context = {
+        'user': user
+    }
+
+    return render(request, 'profile.html', context)
 
 def matches(request):
     
