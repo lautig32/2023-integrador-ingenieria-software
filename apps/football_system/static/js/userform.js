52a65dc318ff09086d3a34c80f355845d7d14548
magugomez <magugomez09@gmail.com>
2023-09-03 19:43:23 -0300

    Update generic data template

--- apps/football_system/static/js/userform.js
+++ apps/football_system/static/js/userform.js
@@ -14,7 +14,9 @@ document.getElementById("btn_login").addEventListener("click", function() {
 function validateUsername(){
   var usernameInput = document.getElementById("username");
   var usernameError = document.getElementById("username_error");
-  if(usernameInput.value == list.value){
+  
+  
+  if(usernameInput.value == link.value){
     usernameError.style.display = "El nombre de usuario ya existe.";
   }
   else{
@@ -28,14 +30,17 @@ var passwordInput = document.getElementById("password");
 var confirmPasswordInput = document.getElementById("confirm_password");
 var passwordError = document.getElementById("password_error");
 
+confirmPasswordInput.addEventListener("input", validatePassword);
+
 function validatePassword() {
   if (passwordInput.value !== confirmPasswordInput.value) {
-      text = "Las contraseñas no coinciden.";
+    confirmPasswordInput.setCustomValidity("Las contraseñas no coinciden.");
   } else {
-      
+    confirmPasswordInput.setCustomValidity("");
   }
 }
-confirmPasswordInput.addEventListener("input", validatePassword);
+passwordInput.addEventListener("input", validatePasswords);
+
 
 //accion bnt_signup
 document.getElementById("signupBtn").addEventListener("click", function() {
@@ -49,4 +54,36 @@ document.getElementById("signupBtn").addEventListener("click", function() {
   } else {
     alert("Por favor, completa todos los campos.");
   }
+});
+
+
+//js para visualizar contraseña
+const passwordInput = document.querySelector(" pass-field input");
+const eyeIcon = document .querySelector(" pass-field i");
+const requirementList = document.querySelectorAll(".requirement-list li");
+
+const requirements = [
+  { regex: /.{8,}/, index: 0 }, // Minimum of 8 characters
+]
+
+passwordInput.addEventListener("keyup", (e) => {
+  requirements.forEach(item => {
+      // Check if the password matches the requirement regex
+      const isValid = item.regex.test(e.target.value);
+      const requirementItem = requirementList[item.index];
+
+      // Updating class and icon of requirement item if requirement matched or not
+      if (isValid) {
+          requirementItem.classList.add("valid");
+          requirementItem.firstElementChild.className = "fa-solid fa-check";
+      } else {
+          requirementItem.classList.remove("valid");
+          requirementItem.firstElementChild.className = "fa-solid fa-circle";
+      }
+  });
+});
+
+eyeIcon.addEventListener("click", () => {
+  passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+  eyeIcon.className = `fa-solid fa-eye${passwordInput.type === "password" ? "" : "-slash"}`;
 });
